f4c2cc4041df4b7f6a974a4050d28015d158ea47
ShujiShimada <cocoa.s.simada@gmail.com>
2021-08-29 15:43:29 +0000

    [finish]4-6

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '72a23e5acf77627fffe1c7297fa35a51295d029d78c2396b8b024cdc3618ce93ea52b0fb206a830a7d55f0f6c92455d017d248e3b5676cc0c801f2186e36ded8'
